7d226b49459f149cb93069c612e35cd6681f9b8f
Requi <main@requi.dev>
2025-09-06 17:12:36 +0200

    Fix isPositionClear force loading chunks exponentially

--- Common/src/main/java/com/nyfaria/nyfsspiders/common/entity/movement/AdvancedClimberPathNavigator.java
+++ Common/src/main/java/com/nyfaria/nyfsspiders/common/entity/movement/AdvancedClimberPathNavigator.java
@@ -461,7 +461,7 @@ public class AdvancedClimberPathNavigator<T extends Mob & IClimberEntity> extend
 
     protected boolean isPositionClear(int x, int y, int z, int sizeX, int sizeY, int sizeZ, Vec3 start, double dx, double dz, double minDotProduct, Direction.Axis ax, Direction.Axis ay, Direction.Axis az) {
         for (BlockPos pos : BlockPos.betweenClosed(new BlockPos(x, y, z), new BlockPos(x + sizeX - 1, y + sizeY - 1, z + sizeZ - 1))) {
-            if (level.isLoaded(pos)) continue;
+            if (!level.isLoaded(pos)) continue;
             double offsetX = swizzle(pos.getX(), pos.getY(), pos.getZ(), ax) + 0.5D - swizzle(start, ax);
             double pffsetZ = swizzle(pos.getX(), pos.getY(), pos.getZ(), az) + 0.5D - swizzle(start, az);
 
